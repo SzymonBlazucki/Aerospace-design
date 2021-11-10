ed12e2ee4d9c6c7155e2c0b9e6c151768593d9eb
SzymonBlazucki <szymon.blazucki@gmail.com>
2021-11-10 14:56:14 +0100

    quick update

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
+import reader
+import scipy as sp
+import matplotlib.pyplot as plt
+from scipy import interpolate
+import numpy as np
+
+
+def interp(x, y):
+    f = interpolate.interp1d(x, y, kind='cubic', fill_value='extrapolate')
+    return f
+
+testFirstTable, testSecondTable = reader.readXLFR('xlfrData/test.csv')
+
+
 
 class Forces:
-
+    def __init__(self, first, second):
+        self.Cl = interp(first['y-span'], first.Cl)
     def axialForcePlot(self):
         pass
+
     def shearForcePlot(self):
         pass
-    def interp(self):
-        pass
+
     def torquePlot(self):
         pass
+
     def bendingMomentPlot(self):
         pass
 
+testForces = Forces(testFirstTable,testSecondTable)
+span =
+#print(testForces.Cl(np.linspace(0,10,100)))
+plt.plot()
 class Wing:
     pass

72cd03a51384cd9cc83e9a9af8dbddf4d64d7de0
SzymonBlazucki <szymon.blazucki@gmail.com>
2021-11-10 14:18:38 +0100

    structure defined

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 
+class Forces:
+
+    def axialForcePlot(self):
+        pass
+    def shearForcePlot(self):
+        pass
+    def interp(self):
+        pass
+    def torquePlot(self):
+        pass
+    def bendingMomentPlot(self):
+        pass
+
 class Wing:
     pass

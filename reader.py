0c98aedf42d342e886fbf39dc393b99d49bb1925
enesbkoca <enesbkoca@gmail.com>
2021-11-10 13:52:56 +0100

    added class Wing

--- reader.py
+++ reader.py
@@ -5,4 +5,3 @@ import scipy as sp
 firstTable = pd.read_csv('xlfrData/test.csv', skipinitialspace=True, encoding_errors='ignore', skiprows=20, nrows=38)
 secondTable = pd.read_csv('xlfrData/test.csv', skipinitialspace=True, encoding_errors='ignore', skiprows=62, skip_blank_lines=True)
 secondTable = secondTable[pd.notnull(secondTable.Nz)]
-

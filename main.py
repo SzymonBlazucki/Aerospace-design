81029e0bfb3986ee25acd3b4c96b0137c991867e
SzymonBlazucki <szymon.blazucki@gmail.com>
2021-11-10 16:52:46 +0100

    working shear

--- main.py
+++ main.py
@@ -34,9 +34,11 @@ class Forces:
         pass
 
     def shearForce(self, x):
-        for
-        shearDist = quad(self.lift, x, self.b2)
-        return shearDist
+        out = []
+        for y in x:
+            shearDist, trash = quad(self.lift, y, self.b2)
+            out.append(shearDist)
+        return np.array(out)
 
     def torque(self):
         pass
@@ -47,23 +49,14 @@ class Forces:
         pass
 
 testForces = Forces(testFirstTable, testSecondTable, 10, 25, 10)
-span = np.linspace(0, 25, 10000)
-trash1, teash2 = quad(testForces.lift, 5, 25)
-#print(trash1)
-#spanDiff = span[1:] - span[:-1]
-# print(testForces.Cl(np.linspace(0,10,100)))
-# plt.plot(span, testForces.cord(span))
-#print(testForces.shearForce(np.array([1,2])))
-#plt.plot(span, testForces.shearForce(span))
-#plt.show()
+span = np.linspace(0, 25, 100)
 
-fun2int = lambda x, a: np.sqrt(x+a)
-intfun = lambda a: quad(fun2int, 0, 4, args=(a))[0]
-vec_int = np.vectorize(intfun)
-vec_int(np.linspace(0,2,5))
-print(vec_int)
+plt.plot(span, testForces.lift(span))
+plt.show()
 
-# print(span[1:] - span[:-1] )
+plt.plot(span, testForces.shearForce(span))
+plt.show()
+
 
 class Wing:
     pass

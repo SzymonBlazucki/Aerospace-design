9acab708e80e33ba292dd119c4fd8cd0062e3405
enesbkoca <enesbkoca@gmail.com>
2021-11-13 23:02:34 +0100

    Started with torque distribution

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-# Torque stuff, change span to negative
-# Shear and bending in the other direction (drag) ?
+# Change span to negative. Proposition: mirror all the graphs for a lazy solution
+# Shear and bending in the other direction (drag)
+# Include Cm (and Cd) in torque
+# Set a coordinate system and possibly a sketch of what is what
 
 
-import math
-import reader
-import scipy as sp
+
+import math, reader
 import matplotlib.pyplot as plt
-from scipy import interpolate
+from scipy import interpolate, signal
 from scipy.integrate import quad
 import numpy as np
 
@@ -31,13 +32,14 @@ class Forces:
         self.Cl = interp(first['y-span'], first.Cl)
         self.chord = interp(first['y-span'], first.Chord)
         self.lCd = interp(first['y-span'], first.ICd)
+        self.xCp = interp(first['y-span'], first.XCP)
         self.b2 = bHalf
         self.angle = math.radians(10)
         self.shearFunction = None
 
     def lift(self, x):
         L = self.Cl(x) * 0.5 * 1.225 * self.v ** 2 * self.chord(x)  # constant rho assumed, update later
-        # L = self.Cl(x) * 0 + self.cord(x) * 0 + 1 #To get a straight lift dist.
+        #L = np.ones_like(x) #To get a straight lift dist.
         # print(np.sum(L))
         return L
 
@@ -48,12 +50,12 @@ class Forces:
         out = []
         for y in x:
             shearDist, trash = quad(self.lift, y, self.b2)
+            # if y < 15: # Including the engine
+            #     shearDist -= 5000*9.81
             out.append(shearDist)
+
         self.shearFunction = interp(x, np.array(out))
         return np.array(out)
-
-    def torque(self):
-        pass
 
     def bendingMoment(self, x):
         if self.shearFunction == None:
@@ -65,9 +67,21 @@ class Forces:
         # self.shearFunction = interp(x, np.array(out))
         return np.array(out)
 
+    def torque(self, x):
+        out = []
+        def d(x): # Distance between wb centroid and xcp
+            return self.xCp(x) - 0.3755 * self.chord(x)
+        def h(x): # Function of distance * shear
+            return interp(x, d(x) * self.shearFunction(x))
+        for y in x:
+            torqueDist, trash = quad(h(x), y, self.b2)
+            out.append(torqueDist)
+
+        return np.array(out)
+
 
 testForces = Forces(testFirstTable, testSecondTable, 10, 25, 10)
-span = np.linspace(0, 25, 100)
+span = np.linspace(0, 25, 101)
 
 # plt.plot(span, testForces.lift(span))
 # plt.show()
@@ -79,6 +93,7 @@ span = np.linspace(0, 25, 100)
 plotter(span, testForces.lift(span), 'Span [m]', 'Lift per span [N/m]')
 plotter(span, testForces.shearForce(span), 'Span [m]', 'Shear force [N]')
 plotter(span, testForces.bendingMoment(span), 'Span [m]', 'Bending moment [N*m]')
+plotter(span, testForces.torque(span), 'Span [m]', 'Torque [N*m]')
 
 
 class Wing:

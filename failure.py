2de4a1da45323f8deb06def297017ee9318bacf2
JoshuaBog09 <joshua.bogaert@gmail.com>
2021-12-01 15:49:36 +0100

    Failure due to torsion formula extended

--- failure.py
+++ failure.py
@@ -2,6 +2,8 @@
 
 #joshua
 #josh
+import numpy as np
+
 
 class Failure:
     def __init__(self, forces, wingbox, stringer):
@@ -9,6 +11,18 @@ class Failure:
         self.Wingbox = wingbox
         self.Stringer = stringer
 
+    def yDistance(self, x):
+        # Get the y locations of the stringer
+        stringerY = self.Stringer.activeStringers(self.Stringer.totalStr, x) * self.Stringer.YPos()
+        rows, cols = stringerY.shape
+        yCentroid = np.tile(np.array([self.Wingbox.yBarWingbox(x)]).transpose(), (1, cols))
+        return (yCentroid - stringerY)
+
     def StressTorsion(self, x):
-        constTorsion = - self.Forces.bendingMoment(x)/self.Wingbox.momentInertiaX(x)
-        return constTorsion
+        # calculate the stress due to torsion without y location
+        consttorsion = - self.Forces.bendingMoment(x)/self.Wingbox.momentInertiaX(x)
+
+        # get the y location of the stringers w.r.t the neutral axis
+        print(f"The locations {self.yDistance(x)[0]}")
+        return consttorsion
+

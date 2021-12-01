a6f21f74287cf1ff6888a86a447a99a5cec24221
enesbkoca <enesbkoca@gmail.com>
2021-12-01 14:31:05 +0100

    class Failure

--- failure.py
+++ failure.py
@@ -2,3 +2,9 @@
 
 #joshua
 #josh
+
+class Failure:
+    def __init__(self, forces, wingbox, stringer):
+        self.Forces = forces
+        self.Wingbox = wingbox
+        self.Stringer = stringer

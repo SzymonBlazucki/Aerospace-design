5ceb2ca6d3098b2ffa72d6a5dca71759295de7c9
enesbkoca <enesbkoca@gmail.com>
2021-12-16 19:51:19 +0100

    Stupid function, don't touch

--- failure.py
+++ failure.py
@@ -1,1 +1,224 @@
-#please add stuff here
+import math
+from constants import E, K, v, k_s, k_c, k_v, k_ic, a, sigma_y
+import numpy as np
+
+
+class Failure:
+    def __init__(self, forces, wingbox, stringer):
+        self.Forces = forces
+        self.Wingbox = wingbox
+        self.Stringer = stringer
+        self.tAft = self.Stringer.thickness[0]
+        self.tBot = self.Stringer.thickness[1]
+        self.tFront = self.Stringer.thickness[2]
+        self.tTop = self.Stringer.thickness[3]
+
+    # Stringer buckling at the root (root has the critical stress due to bending)
+    def stressBending(self, x):
+        # calculate the stress due to torsion without y location at the root
+        constbending = -self.Forces.bendingMoment(x)[0] / self.Wingbox.momentInertiaX(x)[0]
+
+        # get the y location of the stringers w.r.t the neutral axis and convert it to [m]
+        yCentroid, stringerY = self.Wingbox.strYDistance(x)
+        yDistance = (stringerY[0] - yCentroid[0]) * self.Forces.chord(x)[0]
+
+        # output result for stress at all stringers at the root
+        out = constbending * yDistance
+        # print(f"the stresses for stringers {out}")
+        # print(f"stressbending{out}")
+        return out
+
+    def testStress(self, x):
+        index = self.indexCritical(x)
+        # print(f"the index of critical stringer is {index}")
+        ylocation = self.Stringer.YPos()[index] * self.Forces.chord(x)
+        stress = abs(-self.Forces.bendingMoment(x) / self.Wingbox.momentInertiaX(x) * ylocation)
+        return stress
+
+    def stressBendingGen(self, x):
+        # calculate the stress due to torsion without y location at the root
+        constbending = -self.Forces.bendingMoment(x) / self.Wingbox.momentInertiaX(x)
+
+        # get the y location of the stringers w.r.t the neutral axis and convert it to [m]
+        yCentroid, stringerY = self.Wingbox.strYDistance(x)
+        yDistance = (stringerY[0] - yCentroid[0]) * self.Forces.chord(x)[0]
+
+        # output result for stress at all stringers at the root
+        # out = constbending * yDistance
+
+        return np.array(list(map(lambda i: np.amax(i), stringerY)))
+
+    def generalisedStress(self, x):
+        y = self.Wingbox.steinerTerm(x)
+        return self.Forces.bendingFunction(x) * y / self.Wingbox.momentInertiaX(x)
+
+    # return the critical column buckling stress based on inputs
+    def columnBuckling(self, x):
+        # Create boolean array based on stress type (compression = 1, tensile =0)
+        cforceboolean = np.where(self.stressBending(x) > 0, np.inf, 1)
+        #print(f"buckling{out}")
+        ribs = self.Wingbox.ribs
+        longerRibs = np.diff(ribs)
+        #ribsForOut = np.array(list(map(lambda i: max(longerRibs[ribs < i]), self.Forces.span)))
+        #out = cforceboolean * (math.pi ** 2 * K * E * self.Stringer.strIxx) / \
+        #      ((longerRibs ** 2 * self.Stringer.areaArr))
+        print(longerRibs)
+        a = 0
+        max = 10000000000000
+        for i in longerRibs:
+            minIn = np.amin((math.pi ** 2 * K * E * self.Stringer.strIxx) / ((i ** 2 * self.Stringer.areaArr)))
+            print(minIn)
+            if minIn < max:
+                a = (math.pi ** 2 * K * E * self.Stringer.strIxx) / ((i ** 2 * self.Stringer.areaArr))
+        print(a)
+        return a
+
+
+
+
+    def zero_runs(a):
+        # Create an array that is 1 where a is 0, and pad each end with an extra 0.
+        iszero = np.concatenate(([0], np.equal(a, 0).view(np.int8), [0]))
+        absdiff = np.abs(np.diff(iszero))
+        # Runs start and end where absdiff is 1.
+        ranges = np.where(absdiff == 1)[0].reshape(-1, 2)
+        return ranges
+
+    def ab(self, x):
+        rb = self.Wingbox.ribs
+        outRb = np.roll(rb, -1)  # implement diff somewhere
+        filter = np.array(range(0, len(rb)))
+        length = rb[1:] - rb[:-1]
+        outB = np.array(list(map(lambda i: max(outRb[rb < i]), x)))
+        stringersTot = self.Stringer.activeStringers(self.Stringer.totalStr, x)
+        stringersArr = stringersTot[:, -len(self.Stringer.topStr):]
+        fullCol = np.zeros(len(stringersArr[:, 0]))  # column of zeros
+        padded = np.concatenate(([fullCol * 0], np.transpose(np.equal(stringersArr, 0)), [fullCol * 0]),
+                                axis=0).T  # pads left and right column of zeros
+        ones = np.abs(np.diff(padded))  # gives 1 at difference between 0 and 1
+        outBn = np.array(list(map(lambda i: np.amax(np.append(np.diff(np.where(i == 1)[0].reshape(-1, 2)), [1])),
+                                  ones)))  # calculates longest interval between stringers in the unit of stringer
+        # spacing
+        outA = np.array(list(map(lambda i: length[max(filter[rb < i])], x)))  # calculates "a" side of the pannel
+        return outA / (0.45 * self.Forces.chord(outB) * outBn / len(stringersArr[0])), \
+               outA / (0.45 * self.Forces.chord(outB) * outBn / len(stringersArr[0]))  # first aft then front
+
+    def tb(self, x):  # to be modified, for now good enough
+        rib = self.Wingbox.ribs
+        outRb = np.roll(rib, -1)
+        # output = 0.0662 * self.Forces.chord(np.array(list(map(lambda i: max(outRb[rib < i]), self.Forces.span))))
+        rb = self.Wingbox.ribs
+        outRb = np.roll(rb, -1)  # implement diff somewhere
+        outB = np.array(list(map(lambda i: max(outRb[rb < i]), x)))
+        stringersTot = self.Stringer.activeStringers(self.Stringer.totalStr, x)
+        stringersArr = stringersTot[:, -len(self.Stringer.topStr):]
+        fullCol = np.zeros(len(stringersArr[:, 0]))  # column of zeros
+        padded = np.concatenate(([fullCol * 0], np.transpose(np.equal(stringersArr, 0)), [fullCol * 0]),
+                                axis=0).T  # pads left and right column of zeros
+        ones = np.abs(np.diff(padded))  # gives 1 at difference between 0 and 1
+        outBn = np.array(list(map(lambda i: np.amax(np.append(np.diff(np.where(i == 1)[0].reshape(-1, 2)), [1])),
+                                  ones)))
+        return self.tBot / (0.45 * self.Forces.chord(outB) * outBn / len(stringersArr[0])), self.tTop / (
+                    0.45 * self.Forces.chord(outB) * outBn / len(stringersArr[0]))
+
+    def b(self):
+        rib = self.Wingbox.ribs
+        outRb = np.roll(rib, -1)
+        output = np.array(list(map(lambda i: max(outRb[rib < i]), self.Forces.span)))
+        return [0.0662 * self.Forces.chord(output),  # aft
+                0.0653 * self.Forces.chord(output)]  # front
+
+    def skinBuckling(self, x):
+        # allStress = math.pi ** 2 * k_c * E * self.tb(x) ** 2 / (12 * (1 - v ** 2))  # check that
+        top = math.pi ** 2 * k_c(self.ab(x)[0]) * E / 12 / (1 - v ** 2) * self.tb(x)[
+            1] ** 2  # math.pi ** 2 * k_c * E / 12 / (1 - v ** 2) * self.tb(x)[0] ** 2, \
+
+        return top, self.tTop
+
+    def webBuckling(self):
+        b = self.b()
+        # print(f"b{b}")
+        aft, front = [math.pi ** 2 * k_s(self.ab(self.Forces.span)[0]) * E * (self.tAft / b[0]) ** 2 / 12 / (1 - v ** 2),
+                      math.pi ** 2 * k_s(self.ab(self.Forces.span)[0]) * E * (self.tFront / b[1]) ** 2 / 12 / (1 - v ** 2)]
+
+        if aft[0] > front[0]:
+            # print(f"aft{aft}")
+            return aft, self.tAft
+        else:
+            # print(f"front{front}")
+            return front, self.tFront
+
+    def crackStress(self):
+        # return np.concatenate((aft, front)).max()  # return the highest stress value
+        return k_ic / math.sqrt(math.pi * a)
+
+    def shearStressForce(self, x):
+        aft, front = self.b()
+        average = self.Forces.shearForce(x) / (aft * self.tAft + front * self.tFront)
+        return average * k_v
+
+    def shearFlowTorque(self, x):
+        stress = self.Forces.torque(x) / (2 * self.Wingbox.enclosedArea(x))
+        # print(f"stress{stress}")
+        return stress
+
+    def marginWeb(self, x):
+        failure, t = self.webBuckling()
+        stress = self.shearFlowTorque(x) * t + self.shearStressForce(x)
+        margin = failure / stress
+        # print(f"marginweb{margin}")
+        return margin
+
+    def marginSkin(self, x):
+        index = self.indexCritical(x)
+        # print(f"the index of critical stringer in compression is {index}")
+        ylocation = self.Stringer.YPos()[index] * self.Forces.chord(x)
+        stress = abs(-self.Forces.bendingMoment(x) / self.Wingbox.momentInertiaX(x) * ylocation)
+        critical_stress = self.skinBuckling(x)[0]
+        return critical_stress / stress
+        # return 1 - self.stressBending(x)[0] / self.skinBuckling(x)[0]  # for skin buckling I always want top, fix skin
+        # buckling and stress bending - I want stress at every point not only at the root
+
+    def marginCrack(self, x):
+        index = self.indexCriticalTens(x)
+        # print(f"the index of critical stringer in tension is {index}")
+        ylocation = self.Stringer.YPos()[index] * self.Forces.chord(x)
+        stress = self.Forces.bendingMoment(x) / self.Wingbox.momentInertiaX(x) * ylocation
+        return self.crackStress() / abs(stress)
+
+    def indexCritical(self, x):
+        out = - self.stressBending(x) / self.columnBuckling(x)  # it is dividing be zero sometimes, please fix that
+        for i in self.Stringer.cornerIndex:
+            out[i] = 0
+        critical_point = np.where(out > 0, out, -np.inf).argmax()
+        # print(f"criticalpoint{critical_point}")
+        return critical_point
+
+    def indexCriticalTens(self, x):
+        out = - self.stressBending(x)  # it is dividing be zero sometimes, please fix that
+        # print(out)
+        for i in self.Stringer.cornerIndex:
+            out[i] = 0
+        # print(out)
+        critical_point = np.where(out < 0, out, np.inf).argmin()
+        return critical_point
+
+    # return the margin due to bending stress of the critical stringer
+    def marginStringer(self, x):
+        index = self.indexCritical(x)
+        # print(f"the index of critical stringer is {index}")
+        ylocation = self.Stringer.YPos()[index] * self.Forces.chord(x)
+        stress = -self.Forces.bendingMoment(x) / self.Wingbox.momentInertiaX(x) * ylocation
+        critical_stress = - self.columnBuckling(x)[index]
+        # critical_stress = - self.rankineGordon(x)[index]
+
+        margin = critical_stress / stress
+        # print(f"stress{stress}")
+        # print(f"margin{margin}")
+        # print(f"criticalstress{critical_stress}")
+        return margin
+
+    def rankineGordon(self, x):
+        a = sigma_y / math.pi ** 2 / E
+        stress_yield = sigma_y / (1 + a * self.Stringer.totalStr ** 2 * self.Stringer.areaArr / self.Stringer.strIxx)
+        return stress_yield
--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import numpy as np
 from constants import cld, zeroCl, tenCl, zeroAngleFirstTable, tenAngleFirstTable
 import time
 
-zeroTIme = time.time()
-start = time.time()
+prog_start = time.time()
 
+
+def plotter(x, y, xLabel, yLabel, logarithmic=False, lowerlimit=None, upperlimit=None):
+    plt.plot(x, y(x), label=yLabel)
 
     if logarithmic:
         plt.yscale("log")
@@ -39,38 +41,66 @@ topStringers = [16, 18, 16, 16]
 
 botType = [1, 1, 1]
 botStringers = [13, 9, 6]
-strng = Stringer(strArea, np.array(topStringers), np.array(botStringers), wbthickness=wbThickness)
+
+rib_pitch = 0.35  # space between ribs in meters (range of 0.2-1)
+ribs = np.array([0, 0.2, 0.5, 1, 1.3, 1.8, 5, 7, 9, 13, 17, 20, 25, 28])  # np.linspace(0, 28, int(28/rib_pitch + 1))
+
 velocity = 250
 angle = 10
 
+start = time.time()
+eng = Engine()
+end = time.time()
+print(f"Time to load Engine: {end - start}s")
+
+start = time.time()
+strng = Stringer(strIxx, topType, botType, strArea, np.array(topStringers), np.array(botStringers),
+                 wbthickness=wbThickness)
+end = time.time()
+print(f"Time to load Stringer: {end - start}s")
+
+start = time.time()
 testForces = Forces([zeroAngleFirstTable, tenAngleFirstTable],
                     freeVel=velocity, bHalf=28,
                     AoA=math.asin((cld - zeroCl) / (tenCl - zeroCl) * math.sin(math.radians(10))), xCentroid=0.3755,
                     # might've changed
-                    engine=eng, spanSteps=51, stringer=strng, density=2700)
+                    engine=eng, spanSteps=101, stringer=strng, density=2700)  # FOr final results keep steps high
 end = time.time()
-print(end - start)
+print(f"Time to load Forces: {end - start}s")
+
 start = time.time()
 wb = Wingbox(forces=testForces, stringer=strng, sweep=27, ribs=ribs, rib_pitch=rib_pitch)
 end = time.time()
-print(end - start)
+print(f"Time to load Wingbox: {end - start}s")
+
 start = time.time()
 failuremode = Failure(forces=testForces, wingbox=wb, stringer=strng)
 end = time.time()
-print(end - start)
+print(f"Time to load Failure: {end - start}s")
+
 start = time.time()
-plotter(testForces.span, testForces.weight, 'Span [m]', 'Weight per span [N/m]')
-plotter(testForces.span, testForces.drag, 'Span [m]', 'Drag per span [N/m]')
-plotter(testForces.span, testForces.shearFunction, 'Span [m]', 'Shear Diagram [N]')
-#
-plotter(testForces.span, wb.momentInertiaX, 'Span [m]', 'Moment of Inertia [m^4]')
-plotter(testForces.span, testForces.bendingFunction, 'Span [m]', 'Bending Moment [N*m]')
-plotter(testForces.span, wb.bendingDisplacement, 'Span [m]', 'Horizontal Displacement [m]')
-#
-plotter(testForces.span, wb.torsionalStiffness, 'Span [m]', 'Torsional Stiffness [m^4]')
-plotter(testForces.span, testForces.twistFunction, 'Span [m]', 'Torque [N*m]')
-plotter(testForces.span, wb.twistDisplacement, 'Span [m]', 'Twist Displacement [deg]')
+
+plotter(testForces.span, failuremode.marginStringer, 'Span [m]', 'MoS Stringer', logarithmic=True, lowerlimit=1)
+plotter(testForces.span, failuremode.marginWeb, 'Span [m]', 'MoS Web', logarithmic=True, lowerlimit=1)
+plotter(testForces.span, failuremode.marginSkin, 'Span [m]', 'MoS Skin', logarithmic=True, lowerlimit=1)
+plotter(testForces.span, failuremode.marginCrack, 'Span [m]', 'MoS Crack', logarithmic=True, lowerlimit=1)
+
+# plotter(testForces.span, testForces.chord, 'Span [m]', 'Chord [m]')
+# plotter(testForces.span, testForces.weight, 'Span [m]', 'Weight per span [N/m]')
+# plotter(testForces.span, testForces.drag, 'Span [m]', 'Drag per span [N/m]')
+# plotter(testForces.span, testForces.lift, 'Span', 'Lift per span [N/m]')
+# plotter(testForces.span, testForces.verticalForce, 'Span [m]', 'Vertical force per span [N/m]')
+# plotter(testForces.span, testForces.shearFunction, 'Span [m]', 'Shear Diagram [N]')
+
+# plotter(testForces.span, wb.momentInertiaX, 'Span [m]', 'Moment of Inertia [m^4]')
+# plotter(testForces.span, testForces.bendingFunction, 'Span [m]', 'Bending Moment [N*m]')
+plotter(testForces.span, wb.bendingDisplacement, 'Span [m]', 'Horizontal Displacement [m]', upperlimit=4.2)
+
+# plotter(testForces.span, wb.torsionalStiffness, 'Span [m]', 'Torsional Stiffness [m^4]')
+# plotter(testForces.span, testForces.twistFunction, 'Span [m]', 'Torque [N*m]')
+plotter(testForces.span, wb.twistDisplacement, 'Span [m]', 'Twist Displacement [deg]', upperlimit=10)
+
 end = time.time()
-print('total time')
-print(end - zeroTIme)
-start = time.time()
+print(f"Time to load Graphs: {end - start}s")
+prog_end = time.time()
+print(f"Total time to run the whole program is {prog_end - prog_start}s")
